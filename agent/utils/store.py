645164f3e9aa3b47782691091596edf351477daf
Marek Przechrzta <mprzechrzta@gmail.com>
2025-06-05 21:15:11 +0200

    Change rag_store ports

--- agent/utils/store.py
+++ agent/utils/store.py
@@ -2,7 +2,7 @@ import requests
 
 
 def search_websites(query: str):
-    url = "http://127.0.0.1:8000/websites/search"
+    url = "http://127.0.0.1:8080/websites/search"
     data = {"query": query} 
     response = requests.post(url, json=data)
     res = response.json()
@@ -10,21 +10,21 @@ def search_websites(query: str):
     return res.get("serialized", ""), res.get("retrieved_docs", "")
 
 def create_order(payload):
-    url = "http://127.0.0.1:8000/orders"
+    url = "http://127.0.0.1:8080/orders"
     response = requests.post(url, json=payload)
     res = response.json()
     print(res)
     return res
 
 def create_message(payload):
-    url = "http://127.0.0.1:8000/messages"
+    url = "http://127.0.0.1:8080/messages"
     response = requests.post(url, json=payload)
     res = response.json()
     print(res)
     return res
 
 def list_orders():
-    url = "http://127.0.0.1:8000/orders"
+    url = "http://127.0.0.1:8080/orders"
     response = requests.get(url)
     res = response.json()
     print(res)
